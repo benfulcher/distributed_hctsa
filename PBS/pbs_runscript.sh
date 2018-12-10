139a7943aa82e0c2f7bf5bf59ddb9a1c9acd9bf4
BrendanJHarris <42064608+BrendanJHarris@users.noreply.github.com>
2018-12-10 14:21:27 +1100

    Update pbs_runscript.sh

--- PBS/pbs_runscript.sh
+++ PBS/pbs_runscript.sh
@@ -7,10 +7,10 @@
 #PBS -q physics
 #PBS -l nodes=1:ppn=1
 # Set your minimum acceptable walltime, format: day-hours:minutes:seconds
-#PBS -l walltime=00:20:00
+#PBS -l walltime=00:01:00
 # Email user if job ends or aborts
 #PBS -m ea
-#PBS -M bhar9988@uni.sydney.edu.au
+#PBS -M ben.fulcher@physics.sydney.edu.au
 #PBS -V
 
 # your commands/programs start here, for example:
