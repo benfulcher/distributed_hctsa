b11528e5c07b1fd5ce7522a44099627a689d18fa
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-07-06 12:29:44 +1000

    Some shells need cp rather than copyfile

--- CombineBatchFiles.m
+++ CombineBatchFiles.m
@@ -12,12 +12,17 @@ directories = files(isDirectory);
 directoryNames = {directories.name};
 numFiles = length(directoryNames);
 
-% Do the first one
-copyfile(fullfile(directoryNames{1},'HCTSA_subset.mat'),'./HCTSA.mat');
+% Do the first one as a seed
+try
+    copyfile(fullfile(directoryNames{1},'HCTSA_subset.mat'),'./HCTSA.mat');
+catch
+    system(sprintf('cp %s ./HCTSA.mat',fullfile(directoryNames{1},'HCTSA_subset.mat')))
+end
 
+% Loop through the rest:
 for i = 2:numFiles
     newFile = fullfile(directoryNames{i},'HCTSA_subset.mat');
-    TS_Combine('HCTSA.mat',newFile,false,false,'HCTSA_combined.mat'); 
+    TS_Combine('HCTSA.mat',newFile,false,false,'HCTSA_combined.mat');
     delete('HCTSA.mat');
     movefile('HCTSA_combined.mat','HCTSA.mat');
 end
