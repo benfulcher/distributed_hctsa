46a5cafec6ff75640eaefd9baa40768c56d7cd46
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-10-27 09:43:34 +1100

    Now merges keeping ts_ids in order

--- CombineBatchFiles.m
+++ CombineBatchFiles.m
@@ -1,18 +1,34 @@
+% CombineBatchFiles
+%-------------------------------------------------------------------------------
+% Script to loop through directories from a distributed batch computation (across
+% ranges of tsids) and combine results into a single HCTSA.mat file.
+%-------------------------------------------------------------------------------
+% Directories are produced by a HCTSA_run.sh script, in the form "tsids_X-Y"
+% for a range of ts_ids from X to Y.
+%===============================================================================
+
 files = dir('./');
 
-% Filter out any directory/file starting with '.'
+% Filter out any directories/files starting with '.'
 for k = length(files):-1:1
     if strcmp(files(k).name(1),'.')
         files(k) = [];
     end
 end
 
+% Retrieve all directories
 isDirectory = [files.isdir];
 directories = files(isDirectory);
 directoryNames = {directories.name};
 numFiles = length(directoryNames);
 
-% Do the first one as a seed
+% Sort by tsids:
+ts_IDs = extractBetween(directoryNames,'tsids_','-');
+ts_IDs = cellfun(@str2num,ts_IDs,'UniformOutput',true);
+[~,ix] = sort(ts_IDs,'ascend');
+directoryNames = directoryNames(ix);
+
+% Copy the first one as a seed for others to combine into
 try
     copyfile(fullfile(directoryNames{1},'HCTSA_subset.mat'),'./HCTSA.mat');
 catch
@@ -20,8 +36,8 @@ catch
 end
 
 % Loop through the rest:
-for i = 2:numFiles
-    newFile = fullfile(directoryNames{i},'HCTSA_subset.mat');
+for f = 2:numFiles
+    newFile = fullfile(directoryNames{f},'HCTSA_subset.mat');
     TS_Combine('HCTSA.mat',newFile,false,false,'HCTSA_combined.mat');
     delete('HCTSA.mat');
     movefile('HCTSA_combined.mat','HCTSA.mat');
